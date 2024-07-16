efa69c1d4e1e78f990bfc7eb564de82e23299b8f
Markizdickabraz <mark.sahaxkiy@gmail.com>
2024-07-16 18:28:21 +0300

    523-7

--- app/code/Vendor/Extension/view/frontend/requirejs-config.js
+++ app/code/Vendor/Extension/view/frontend/requirejs-config.js
@@ -6,6 +6,9 @@ var config = {
             },
             'Magento_Checkout/js/view/summary/abstract-total': {
                 'Vendor_Extension/js/view/summary/abstract-total-mixins': true
+            },
+            'Magento_Checkout/js/model/shipping-rates-validation-rules': {
+                'Vendor_Extension/js/checkout/model/shipping-rates-validation-rules-mixin': true
             }
         }
     }
