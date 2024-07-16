64562cfd94f9b5e0a95dd4e3422b65d105168d98
Markizdickabraz <mark.sahaxkiy@gmail.com>
2024-07-16 17:27:47 +0300

    523-2

---
+++ app/code/Vendor/Extension/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/model/checkout-data-resolver': {
+                'Vendor_Extension/js/model/checkout-data-resolver': true
+            }
+        }
+    }
+};
